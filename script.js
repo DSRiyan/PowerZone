e30f91dec3a46de84a8477e134d053f02abc357b
Mohamed Riyan <162490056+DSRiyan@users.noreply.github.com>
2025-01-20 22:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// FOR LOGIN FORM POPUP
+
+function openLoginForm() {
+    document.body.classList.add("showLLoginForm");
+}
+function closeLoginForm() {
+    document.body.classList.remove("showLLoginForm");
+}
