523ae587e8e8b00e521f6563c6f664e8c7546d4f
Mohamed Riyan <162490056+DSRiyan@users.noreply.github.com>
2025-02-15 23:56:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,31 @@ contactForm.addEventListener('submit', (e) => {
     const batch = document.getElementById('batch').value;
     const method = document.getElementById('method').value;
 
-    thankYouMessage.textContent = `Thank you, ${name} :) You have been enrolled in the ${batch} batch for ${method}.`;
+    thankYouMessage.textContent = `Thank you ${name}, You have been enrolled in the ${batch} batch for ${method}.`;
     thankYouPopup.classList.add('active');
 });
 
 closeThankYou.addEventListener('click', () => {
-    window.open('index.html', '_blank');;
+    location.reload(); // Refresh the page
 });
+
+
+
+
+// Show Scroll to Top Button on Scroll
+window.addEventListener("scroll", function () {
+    const scrollToTopBtn = document.querySelector(".scroll-to-top");
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+// Scroll to Top Function
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
